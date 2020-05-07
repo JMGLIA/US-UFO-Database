e8f2696ced6b01927379a471f4f7929d3e8fedf7
Joseph Geglia <jmgeglia@gmail.com>
2020-05-07 19:26:53 -0400

    logging data from js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+var dataFilter = 0;
+sightings = data
+
+console.log(data);
+
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
