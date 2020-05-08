93e62fef46d88b19ee6b0e7eb4ce96f14c524cfe
Joseph Geglia <jmgeglia@gmail.com>
2020-05-08 17:36:53 -0400

    adding date filter function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,14 +5,37 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 var dataFilter = 0;
-sightings = data
+incidents = data
 
-console.log(data);
-
-data.forEach((ufoSighting) => {
+data.forEach((sighting) => {
     var row = tbody.append("tr");
-    Object.entries(ufoSighting).forEach(([key, value]) => {
+    Object.entries(sighting).forEach(([key, value]) => {
         var cell = tbody.append("td");
         cell.text(value);
     });
 });
+
+d3.select("#filter-btn").on("click", function(){
+    d3.event.preventDefault();
+
+    var inputDate = d3.select("#datetime");
+
+    var inputValue = inputDate.property("value");
+
+    var resultingtable = incidents.filter(incident => incident.datetime === inputValue);
+    console.log(resultingtable)
+    buildFilter(resultingtable);
+
+})
+
+function buildFilter(dataFilter) {
+
+    tbody.html("");
+    dataFilter.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    });
+};
